8c003fc52acad0773b62b6191f93f0a0970fa426
Jan Vornberger <jan@uos.de>
2014-06-17 18:41:04 +0200

    Tweak find nearby screen layout

--- wallet/src/com/hivewallet/androidclient/wallet/ui/AddContactChoiceFragment.java
+++ wallet/src/com/hivewallet/androidclient/wallet/ui/AddContactChoiceFragment.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.text.ClipboardManager;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -73,6 +74,7 @@ public class AddContactChoiceFragment extends DialogFragment
 						
 						int dp8 = (int) (8 * getResources().getDisplayMetrics().density + 0.5f);
 						textView.setCompoundDrawablePadding(dp8);
+						textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.font_size_normal));
 						
 						return view;
 					}
