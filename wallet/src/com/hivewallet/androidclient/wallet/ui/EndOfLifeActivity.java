e0706fbde42e6bcded7f60f33f021a2b20268a7b
Jan Vornberger <jan@uos.de>
2014-10-09 02:51:12 +0200

    Add end of life notice

---
+++ wallet/src/com/hivewallet/androidclient/wallet/ui/EndOfLifeActivity.java
@@ -0,0 +1,38 @@
+package com.hivewallet.androidclient.wallet.ui;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+import com.hivewallet.androidclient.wallet_test.R;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.text.Html;
+import android.widget.TextView;
+
+public class EndOfLifeActivity extends FragmentActivity
+{
+	private static final String SUCCESSOR_APP = "market://details?id=com.hivewallet.hive.cordova";
+	
+	@InjectView(R.id.tv_end_of_life_message) TextView messageView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.end_of_life_activity);
+		ButterKnife.inject(this);
+		
+		messageView.setText(Html.fromHtml(getString(R.string.end_of_life_message)));
+	}
+	
+	@OnClick(R.id.tv_end_of_life_message) void upgradeHive() {
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setData(Uri.parse(SUCCESSOR_APP));
+		startActivity(intent);
+	}
+}
---
+++ wallet/src/com/hivewallet/androidclient/wallet/ui/EndOfLifeNoticeFragment.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.hivewallet.androidclient.wallet.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.hivewallet.androidclient.wallet_test.R;
+
+public final class EndOfLifeNoticeFragment extends Fragment
+{
+	private Activity activity;
+
+	private TextView messageView;
+
+	@Override
+	public void onAttach(final Activity activity)
+	{
+		super.onAttach(activity);
+
+		this.activity = activity;
+	}
+
+	@Override
+	public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState)
+	{
+		messageView = (TextView) inflater.inflate(R.layout.end_of_life_notice_fragment, container);
+		messageView.setText(Html.fromHtml(getString(R.string.end_of_life_notice)));
+		
+		messageView.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				final Intent intent = new Intent(activity, EndOfLifeActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		return messageView;
+	}
+}
